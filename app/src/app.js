0a7afc205c5d9933c00740ae8420ffaef2f7e67a
Felipe Barros <felipelfbarros@gmail.com>
2019-03-28 17:35:49 -0300

    Added botcamp logo

--- app/src/app.js
+++ app/src/app.js
@@ -1,9 +1,15 @@
 import React from "react";
 import BtnBotcamp from "./components/BtnBotcamp";
+import BotcampLogo from "./components/BotcampLogo";
 
 class App extends React.Component {
     render() {
-        return <BtnBotcamp/>
+        return (
+        <React.Fragment>
+            <BotcampLogo/>
+            <BtnBotcamp/>
+        </React.Fragment>
+        );
     }
 }
 
---
+++ app/src/components/BotcampLogo/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import logo from "./botcamp-logo.png";
+
+const BotcampLogo = () => (
+    <img src={logo} alt="Logo do Botcamp"></img>
+);
+
+export default BotcampLogo;
